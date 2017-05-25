387ac0d7bd9d22b25db0b67c373b74a7d24036ec
João Pedro Santos Vidiri <joao.vidiri@gmail.com>
2017-05-25 13:15:57 -0300

    Update backend.php
    1. Comentada a linha 53, dado que os espaços não precisam ser contados.
    2. Adicionei um verificação para que a função não adicione um newline na primeira execução.
    3. Removi o hífen da função, que estava ali apenas para sinalizar as palavras maiores que o limite, mas não fazia parte da especificação.
    4. Adicionei uma verificação para strings $text e $limit.

--- backend.php
+++ backend.php
@@ -10,16 +10,30 @@
   Faça o exercício na linguagem de programação que preferir.
 */
 function printLargeText($text, $limit){
-	//$newLine = "<br>";
-	$newLine = "\n"; // Just for tests in the ubuntu console.
+	$newLine = "<br/>";
+	//$newLine = "\n"; // Just for tests in the ubuntu console.
+
+	if( empty($text) ){
+		echo "Empty text;";
+		echo $newLine;
+		exit();
+	}
+	if (empty($limit) || $limit < 0){
+		echo "Limit invalid;";
+		echo $newLine;
+		exit();
+	}
 
 	$words = explode(" ", $text);//Split by spaces
 	$WordsPrintedSize = 0;
+	$count = 0;
 	foreach ($words as $word){
 		$wordSize = strlen($word);//lets get the word size.
 		$WordsPrintedSize += $wordSize;//lets know how much we will print.
 		if ($wordSize > $limit){			
-			echo $newLine;
+			if ($count != 0){
+				echo $newLine;
+			}
 			echo substr($word, 0, $limit);
 			echo "-";//Not counnting this, since it is the separator.
 			echo $newLine;
@@ -31,7 +45,7 @@ function printLargeText($text, $limit){
 				foreach ($iter as $wordCut){
 					echo $wordCut;
 	 				if ($iter->hasNext()){//if it's not the last we need to wrap again the word.
-						echo "-";//Not counnting this, since it is the separator.
+						//echo "-";
 				    	echo $newLine;
 				    }else{
 				    	$WordsPrintedSize = strlen($wordCut);// in the last we count the last printed size.
@@ -43,22 +57,24 @@ function printLargeText($text, $limit){
 				$WordsPrintedSize = strlen($wordRest);
 			}	
 		}else if ($WordsPrintedSize >= $limit) {
-			echo $newLine;
+			if ($count != 0){
+				echo $newLine;
+			}
 			echo $word;			
 			$WordsPrintedSize = strlen($word); // Count the new line word size
 		}else{
 			echo $word;
 		}
 		echo " ";//Always print a space in the ed of the word printed.
-		$WordsPrintedSize++;//Add the space caracter to the count		
+		//$WordsPrintedSize++;//Add the space caracter to the count
+		$count++;		
 	}
 	echo $newLine;
 }
 
 
-//sample test
-$text = "Suponha que voce tenha uma string com um texto bastante longo. Você quer imprimir na tela todo o texto, mas garantir um limite máximo de N caracteres por linha. Crie uma função que receba dois parâmetros: uma string com o texto e o limite. Imprima o todo o texto, com o máximo de palavras por linha, mas sem nunca extrapolar o limite de caracteres. Se uma palavra não couber na linha e o comprimento dela for menor que o limite de caracteres, ela não deve ser cortada, e sim jogada para a próxima linha. Se a palavra for maior que o limite de caracteres por linha, corte a palavra e continue a imprimi-la na linha seguinte. Não utilize funções prontas, como p.ex. o newLine do PHP. O objetivo deste exercício é que você desenvolva o algoritmo indicado. Faça o exercício na linguagem de programação que preferir."; 
-$limit = 8;
+$text = "1234567890 1234 67890 1234567890 123456789012345 7890";
+$limit = 10;
 
 printLargeText($text, $limit);
 ?>
